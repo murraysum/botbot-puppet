6bc39e59aad04af0fd0d987155972f7b109f6449
Murray Summers <murray.sum@gmail.com>
2013-11-05 23:12:33 +0000

    Tidy up the base manifest.

--- manifests/templates.pp
+++ manifests/templates.pp
@@ -1,11 +1,10 @@
 class base {
-  package {
-    'git-core':       ensure => 'installed';
-    'screen':         ensure => 'installed';
-    'sqlite3':        ensure => 'installed';
-    'libsqlite3-dev': ensure => 'installed';
-    # Required to build listener.c
-    'libasound2-dev': ensure => 'installed';
+  package {'git-core':
+    ensure => 'installed'
+  }
+
+  package {'screen':
+    ensure => 'installed'
   }
 
   package {'bundler':
